c39523658ea0501113eb22d83f4f36e5795da0f3
gabriel <gabriel.leanflin@hotmail.com>
2025-09-17 17:24:36 -0300

    feat: Criando entidade produtos.

---
+++ src/main/java/com/ecommerce_gamer_api/domain/user/Product.java
@@ -0,0 +1,28 @@
+package com.ecommerce_gamer_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Entity(name = "product")
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String category;
+}
