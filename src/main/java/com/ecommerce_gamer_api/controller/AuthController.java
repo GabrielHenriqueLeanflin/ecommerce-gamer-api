88e53f6827834809fa6928258b8c9a81ad1559c8
gabriel <gabriel.leanflin@hotmail.com>
2025-09-17 09:37:57 -0300

    feat: Adicionando dependência do spring security.

---
+++ src/main/java/com/ecommerce_gamer_api/controller/AuthController.java
@@ -0,0 +1,59 @@
+package com.ecommerce_gamer_api.controller;
+
+import com.ecommerce_gamer_api.domain.user.User;
+import com.ecommerce_gamer_api.dto.auth.ErrorResponseDTO;
+import com.ecommerce_gamer_api.dto.auth.LoginRequestDTO;
+import com.ecommerce_gamer_api.dto.auth.RegisterRequestDTO;
+import com.ecommerce_gamer_api.dto.auth.AuthResponseDTO;
+import com.ecommerce_gamer_api.infra.security.TokenService;
+import com.ecommerce_gamer_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDTO body) {
+        Optional<User> userRequest = this.repository.findByEmail(body.email());
+
+        if (userRequest.isPresent() && passwordEncoder.matches(body.password(), userRequest.get().getPassword())) {
+            User user = userRequest.get();
+            String token = tokenService.generateToken(user);
+            return ResponseEntity.ok(new AuthResponseDTO(user.getName(), token));
+        }
+
+        ErrorResponseDTO errorDTO = new ErrorResponseDTO("Usuário ou senha inválidos.", 401, Instant.now());
+        return ResponseEntity.status(401).body(errorDTO);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body) {
+        if (this.repository.findByEmail(body.email()).isPresent()) {
+            ErrorResponseDTO errorDTO = new ErrorResponseDTO("Este email já está cadastrado.", 409, Instant.now());
+            return ResponseEntity.status(409).body(errorDTO);
+        }
+
+        User newUser = new User();
+        newUser.setPassword(passwordEncoder.encode(body.password()));
+        newUser.setEmail(body.email());
+        newUser.setName(body.name());
+        this.repository.save(newUser);
+
+        String token = tokenService.generateToken(newUser);
+        return ResponseEntity.ok(new AuthResponseDTO(newUser.getName(), token));
+    }
+}
